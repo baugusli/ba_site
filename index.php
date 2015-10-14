9a05971a518cb396f32182732ef4800c76257709
poliang13 <bryan_augusli@hotmail.com>
2015-10-14 17:39:54 -0400

    Header color
    color trial and error

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="en">
  <head>
  
-  <!-- IMPT do not delete the first three meta tag. Used for bootstrap -->
+  <!-- IMPT! do not delete the first three meta tag. For bootstrap -->
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
