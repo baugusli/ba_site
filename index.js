04e9ff572a2379c2036f609cdff2b1edf8c32892
Bryan Austin Augusli <baugusli@latinumnetwork.com>
2018-05-20 22:57:55 -0400

    node

---
+++ index.js
@@ -0,0 +1,21 @@
+'use strict';
+const path = require('path');
+const express = require('express');
+const app = express();
+
+const serviceAppPort = 8099;
+const mainPath = path.join(__dirname, '/baugusli');
+
+app.get('/', function(req, res) {
+  res.sendFile(`${mainPath}/index.html`);
+});
+
+// Error handling middleware
+// 500
+app.use(function(err, req, res, next) {
+  console.error(err.stack);
+  res.status(500).send('The application has encountered an unknown error.');
+});
+
+app.use(express.static(mainPath));
+app.listen(serviceAppPort);
